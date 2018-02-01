681287727f9bf461f7c403104bd866d57df4ca71
Erika Thommes <ethommes@scharp.org>
2018-02-01 11:49:32 -0800

    Update to my add-on-packages.csv and add-on-packages-freqtable.csv

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,18 +1,20 @@
 ## create a data frame from data/installed-packages.csv
 ## with, e.g., readr::read_csv() or read.csv()
+df <- read.csv("/Users/ethommes/Desktop/packages-report/data/installed-packages.csv")
 
 ## filter out packages in the default library
 ## keep variables Package and Built
 ## if you use dplyr, code like this will work:
-apt <- ipt %>%
+df2 <- df %>%
   filter(LibPath == .libPaths()[1]) %>%
   select(Package, Built)
 
 ## write this new, smaller data frame to data/add-on-packages.csv
+write.csv(df2, "/Users/ethommes/Desktop/packages-report/data/add-on-packages.csv")
 
 ## make a frequency table of package by the version in Built
 ## if you use dplyr, code like this will work:
-apt_freqtable <- apt %>%
+df2_freqtable <- df2 %>%
   count(Built) %>%
   mutate(prop = n / sum(n))
 
@@ -20,6 +22,7 @@ apt_freqtable <- apt %>%
 ## YES overwrite the files that are there now
 ## they came from me (Jenny)
 ## they are just examples
+write.csv(df2_freqtable, "/Users/ethommes/Desktop/packages-report/data/add-on-packages-freqtable.csv")
 
 ## when this script works, stage & commit it and the csv files
 ## PUSH!
